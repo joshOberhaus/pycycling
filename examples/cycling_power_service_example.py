2da5d43dcf152170178f701ee78a967a7580626d
Josh Oberhaus <josh.oberhaus@gmail.com>
2023-01-03 15:47:48 -0500

    Revert "continue running until input"
    This reverts commit f4f3330a1dc00fcf4f00dc247f9d1ba7cf2f1643.

--- examples/cycling_power_service_example.py
+++ examples/cycling_power_service_example.py
@@ -1,16 +1,7 @@
 import asyncio
 from bleak import BleakClient
-import sys
 
 from pycycling.cycling_power_service import CyclingPowerService
-
-# Borrowing heavily from aioconsole
-async def ainput(string: str) -> str:
-    await asyncio.get_event_loop().run_in_executor(
-            None, lambda s=string: sys.stdout.write(s+' '))
-    return await asyncio.get_event_loop().run_in_executor(
-            None, sys.stdin.readline)
-
 
 async def run(address):
     async with BleakClient(address) as client:
@@ -21,9 +12,7 @@ async def run(address):
         trainer = CyclingPowerService(client)
         trainer.set_cycling_power_measurement_handler(my_measurement_handler)
         await trainer.enable_cycling_power_measurement_notifications()
-        input = ''
-        while not input:
-            await ainput('Press any key to quit:')
+        await asyncio.sleep(30.0)
         await trainer.disable_cycling_power_measurement_notifications()
 
 
